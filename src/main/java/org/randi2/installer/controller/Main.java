457a48bd43ab647ff24b9ca420cbfaf83d45c3d1
Andreas <akudak@stud.hs-heilbronn.de>
2012-01-28 00:33:37 +0100

    git

--- src/main/java/org/randi2/installer/controller/Main.java
+++ src/main/java/org/randi2/installer/controller/Main.java
@@ -111,7 +111,7 @@ public class Main {
 			conf.loadProperties(Language.US, statusService);
 		}
 		start();
-		System.out.println("Git");
+		System.out.println("Git2");
 		mainFrame.repaint();
 	}
 
